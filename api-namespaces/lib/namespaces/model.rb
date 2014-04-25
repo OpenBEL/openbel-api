726595ce44ca2b90a75607119fb587bf3d89644b
Anthony Bargnesi <abargnesi@gmail.com>
2014-04-25 16:53:18 -0400

    back to attr accessors; send writes

--- api-namespaces/lib/namespaces/model.rb
+++ api-namespaces/lib/namespaces/model.rb
@@ -3,8 +3,7 @@ module OpenBEL
 
     class Namespace
 
-      ATTR_MAP = { prefLabel: 'name', prefix: 'prefix' }
-      attr_accessor :uri, :name, :prefix
+      attr_accessor :uri, :prefLabel, :prefix
 
       def ==(other)
         return false if other == nil
@@ -23,24 +22,15 @@ module OpenBEL
           statements.each do |s|
             uri = s.predicate.uri
             attribute = uri.fragment || uri.path[uri.path.rindex('/')+1..-1]
-            if attribute == 'type'
+            if attribute == 'type' and ns.respond_to? :uri=
               ns.send(:uri=, s.subject)
             else
-              attribute = ATTR_MAP[attribute.to_sym]
-              if attribute
+              if ns.respond_to? :"#{attribute}="
                 ns.send(:"#{attribute}=", s.object)
               end
             end
           end
           (ns.respond_to? :uri and ns.uri) ? ns : nil
-        end
-
-        def define_attribute(attribute, value, instance)
-          if not instance.respond_to? attribute
-            define_method(attribute) do
-              instance_variable_get :"@#{attribute}"
-            end
-          end
         end
       end
     end
