9098180174d65fe339f1cd3a53209a9e92990730
Anthony Bargnesi <abargnesi@gmail.com>
2014-06-03 11:52:25 -0400

    added more uri tests; clean up

--- api-namespaces/tests/performance_tests.rb
+++ api-namespaces/tests/performance_tests.rb
@@ -20,29 +20,40 @@ File.open('1000') do |f|
   EQ_VALUE_1000 = f.readlines.map {|x| "value=#{URI.encode(x.chomp)}" }.join('&')
 end
 
+File.open('5000') do |f|
+  EQ_VALUE_5000 = f.readlines.map {|x| "value=#{URI.encode(x.chomp)}" }.join('&')
+end
+
 API_CALLS = [
-  api_uri('/namespaces', nil),
-  api_uri('/namespaces/hgnc', nil),
-  api_uri('/namespaces/hgnc/391', nil),
-  api_uri('/namespaces/hgnc/AKT1', nil),
-  api_uri("/namespaces/hgnc/#{URI.encode('v-akt murine thymoma viral oncogene homolog 1')}", nil),
-  api_uri('/namespaces/hgnc/AKT1/equivalents', nil),
-  api_uri('/namespaces/hgnc/AKT1/orthologs', nil),
-  api_uri("/namespaces/hgnc/equivalents", "result=name&namespace=egid&#{EQ_VALUE_1000}"),
-  api_uri("/namespaces/hgnc/equivalents", "result=resource&namespace=egid&#{EQ_VALUE_1000}")
+  ['All namespaces', api_uri('/namespaces', nil)],
+  ['Namespace', api_uri('/namespaces/hgnc', nil)],
+  ['Value by identifier', api_uri('/namespaces/hgnc/391', nil)],
+  ['Value by name', api_uri('/namespaces/hgnc/AKT1', nil)],
+  ['Value by title', api_uri("/namespaces/hgnc/#{URI.encode('v-akt murine thymoma viral oncogene homolog 1')}", nil)],
+  ['Equivalents for value (by identifier)', api_uri('/namespaces/hgnc/391/equivalents', nil)],
+  ['Equivalents for value (by name)', api_uri('/namespaces/hgnc/AKT1/equivalents', nil)],
+  ['Equivalents for value (by title)', api_uri("/namespaces/hgnc/#{URI.encode('v-akt murine thymoma viral oncogene homolog 1')}/equivalents", nil)],
+  ['Orthologs for value (by identifier)', api_uri("/namespaces/hgnc/391/orthologs", nil)],
+  ['Orthologs for value (by name)', api_uri("/namespaces/hgnc/AKT1/orthologs", nil)],
+  ['Orthologs for value (by title)', api_uri("/namespaces/hgnc/#{URI.encode('v-akt murine thymoma viral oncogene homolog 1')}/orthologs", nil)],
+  ['Equivalent names within namespaces (1000)', api_uri("/namespaces/hgnc/equivalents", "result=name&namespace=egid&#{EQ_VALUE_1000}")],
+  ['Equivalent resources within namespaces (1000)', api_uri("/namespaces/hgnc/equivalents", "result=resource&namespace=egid&#{EQ_VALUE_1000}")],
+  ['Equivalent names within namespaces (5000)', api_uri("/namespaces/hgnc/equivalents", "result=name&namespace=egid&#{EQ_VALUE_5000}")],
+  ['Equivalent resources within namespaces (5000)', api_uri("/namespaces/hgnc/equivalents", "result=resource&namespace=egid&#{EQ_VALUE_5000}")]
 ]
 
 API_CALLS.each do |api_call|
+  (name, uri) = api_call
   Benchmark.bm(80) do |x|
-    x.report(api_call.path + (api_call.query ? "?#{api_call.query.to_s[0..45]}" : '')) {
-      response = HTTP.get_response(api_call)
+    x.report(name) {
+      response = HTTP.get_response(uri)
       code = response.code
       if code != "200"
-        fail "Error #{code} for #{api_call.path}."
+        fail "Error #{code} for #{uri.path}."
       end
       body = response.read_body
       if body.size <= 0
-        fail "Response body is empty for #{api_call.path}."
+        fail "Response body is empty for #{uri.path}."
       end
     }
   end
