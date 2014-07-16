2b691235032a96a2480e6b0127836b890172f816
Anthony Bargnesi <abargnesi@gmail.com>
2014-07-16 17:04:21 -0400

    use $VAR instead of ${VAR}; no var manipulation

--- tools/project/setup-production.sh
+++ tools/project/setup-production.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # move to root, set up environment
-DIR="$(cd "$(dirname "${0}")" && pwd)"/../../
+DIR="$(cd "$(dirname "$0")" && pwd)"/../../
 cd "$DIR" || exit 1
 . ./env.sh || exit 1
 . ./tools/term-colors.sh || exit 1
@@ -21,21 +21,21 @@ current_ruby_version() {
 # install ruby
 if [ "$(current_ruby_version)" != "$OB_RUBY_VERSION" ]; then
     if [ -d "$OB_RUBY_DIR" ]; then
-        printf "${YELLOW}! invalid ruby; removing existing dir: ${OB_RUBY_DIR}${NO_COLOR}\n"
-        rm -fr "${OB_RUBY_DIR}"
+        printf "$YELLOW! invalid ruby; removing existing dir: $OB_RUBY_DIR $NO_COLOR\n"
+        rm -fr "$OB_RUBY_DIR"
     fi
-    printf "${YELLOW}! no local ruby; installing ruby ${OB_RUBY_VERSION}${NO_COLOR}\n"
+    printf "$YELLOW! no local ruby; installing ruby $OB_RUBY_VERSION $NO_COLOR\n"
     ruby-build $OB_RUBY_VERSION $OB_RUBY_DIR || exit 1
 fi
-printf "${GREEN}+ ruby ${OB_RUBY_VERSION} installed${NO_COLOR}\n"
+printf "$GREEN+ ruby $OB_RUBY_VERSION installed$NO_COLOR\n"
 
 # install bundler
 $OB_RUBY_DIR/bin/gem query --name-matches 'bundler' --installed > /dev/null
 if [ $? -eq 1 ]; then
-    printf "${YELLOW}! installing bundler ${NO_COLOR}\n"
+    printf "$YELLOW! installing bundler $NO_COLOR\n"
     $OB_RUBY_DIR/bin/gem install bundler
 fi
-printf "${GREEN}+ bundler installed${NO_COLOR}\n"
+printf "$GREEN+ bundler installed$NO_COLOR\n"
 
 # install app gems through bundler
 $OB_RUBY_DIR/bin/bundle install \
