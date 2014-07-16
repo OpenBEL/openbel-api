4c9ebc2f4481e3ae142a321633808e92d6f39ee4
Anthony Bargnesi <abargnesi@gmail.com>
2014-07-16 16:55:01 -0400

    shell portability; tested against bash and dash
    succeeds when running ``checkbashisms``
    
    refs #5

--- tools/project/setup-production.sh
+++ tools/project/setup-production.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 # move to root, set up environment
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../../
+DIR="$(cd "$(dirname "${0}")" && pwd)"/../../
 cd "$DIR" || exit 1
-. env.sh || exit 1
-. tools/term-colors.sh || exit 1
+. ./env.sh || exit 1
+. ./tools/term-colors.sh || exit 1
 
 # clear RUBYOPT; set on gentoo to auto require a missing gem
 # (https://groups.google.com/forum/#!topic/linux.gentoo.user/Gp5xhTtYoUA)
@@ -15,27 +15,27 @@ current_ruby_version() {
     if [ -f "$OB_RUBY_DIR/bin/ruby" ]; then
         res="$($OB_RUBY_DIR/bin/ruby -e '$stdout.write RUBY_VERSION')"
     fi
-    echo "$res"
+    printf "$res\n"
 }
 
 # install ruby
 if [ "$(current_ruby_version)" != "$OB_RUBY_VERSION" ]; then
     if [ -d "$OB_RUBY_DIR" ]; then
-        echo -e "${YELLOW}! invalid ruby; removing existing dir: ${OB_RUBY_DIR}${NO_COLOR}"
+        printf "${YELLOW}! invalid ruby; removing existing dir: ${OB_RUBY_DIR}${NO_COLOR}\n"
         rm -fr "${OB_RUBY_DIR}"
     fi
-    echo -e "${YELLOW}! no local ruby; installing ruby ${OB_RUBY_VERSION}${NO_COLOR}"
+    printf "${YELLOW}! no local ruby; installing ruby ${OB_RUBY_VERSION}${NO_COLOR}\n"
     ruby-build $OB_RUBY_VERSION $OB_RUBY_DIR || exit 1
 fi
-echo -e "${GREEN}+ ruby ${OB_RUBY_VERSION} installed${NO_COLOR}"
+printf "${GREEN}+ ruby ${OB_RUBY_VERSION} installed${NO_COLOR}\n"
 
 # install bundler
 $OB_RUBY_DIR/bin/gem query --name-matches 'bundler' --installed > /dev/null
 if [ $? -eq 1 ]; then
-    echo -e "${YELLOW}! installing bundler ${NO_COLOR}"
+    printf "${YELLOW}! installing bundler ${NO_COLOR}\n"
     $OB_RUBY_DIR/bin/gem install bundler
 fi
-echo -e "${GREEN}+ bundler installed${NO_COLOR}"
+printf "${GREEN}+ bundler installed${NO_COLOR}\n"
 
 # install app gems through bundler
 $OB_RUBY_DIR/bin/bundle install \
