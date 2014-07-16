cfaead05db8c8abbe7c597e609bdad98ff8a4654
Anthony Bargnesi <abargnesi@gmail.com>
2014-07-16 14:59:37 -0400

    add gems bin/ to PATH; refs #5

--- env.sh
+++ env.sh
@@ -14,5 +14,5 @@ export OB_RUBY_DIR="${OB_RUBY_DIR:=$DIR/vendor/ruby-$OB_RUBY_VERSION}"
 export OB_GEM_DIR="${OB_GEM_DIR:=$DIR/vendor/gems}"
 export OB_GEMBIN_DIR="${OB_GEMBIN_DIR:=$DIR/bin}"
 
-PATH="$OB_RUBY_DIR/bin:$OB_RUBY_BUILD_DIR/bin:$PATH"
+PATH="$OB_RUBY_DIR/bin:$OB_RUBY_BUILD_DIR/bin:$OB_GEMBIN_DIR:$PATH"
 # vim: ts=4 sts=4 sw=4
