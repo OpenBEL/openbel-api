245951655965180ddaa262fd335fa929ff8c912c
Anthony Bargnesi <abargnesi@gmail.com>
2014-08-22 14:38:04 -0400

    avoid Kernel#URI, parse strings, improve perf

--- lib/storage_rdf/extensions/redland/redlander/storage.rb
+++ lib/storage_rdf/extensions/redland/redlander/storage.rb
@@ -11,7 +11,6 @@ module OpenBEL
     def statement_enumerator(subject, predicate, object, options = {})
       subject = uri_node(subject)
       predicate = uri_node(predicate)
-      #object = options[:object_literal] ? literal_node(object) : uri_node(object)
       @model.statements.each(
         :subject => subject,
         :predicate => predicate,
@@ -21,34 +20,30 @@ module OpenBEL
 
     def all(statement)
       [
-        statement.subject.value.to_s,
-        statement.predicate.value.to_s,
-        statement.object.value.to_s
+        statement.subject.to_s[1..-2],
+        statement.predicate.to_s[1..-2],
+        statement.object.to_s[1..-2]
       ]
     end
 
     def subject(statement)
-      statement.subject.value.to_s
+      statement.subject.to_s[1..-2]
     end
 
     def predicate(statement)
-      statement.predicate.value.to_s
+      statement.predicate.to_s[1..-2]
     end
 
     def object(statement)
-      statement.object.value.to_s
+      statement.object.to_s[1..-2]
     end
 
     private
 
     def uri_node(obj)
-      return URI(obj) if obj.is_a? String
-      obj
+      return nil unless obj
+      Redland.librdf_new_node_from_uri_string(Redlander.rdf_world, obj.to_s)
     end
-
-    # def literal_node(obj)
-    #   Redland.librdf_new_node_from_typed_literal($world.world, obj.to_s, '', @xsd_type_uri)
-    # end
   end
 end
 # vim: ts=2 sts=2 sw=2 expandtab
