c79bf10f031e03bcca0de2c98dcfa5323004ca0d
Anthony Bargnesi <abargnesi@gmail.com>
2015-01-16 12:10:15 -0500

    remove commented code; refactor base

--- app/resources/completion.rb
+++ app/resources/completion.rb
@@ -14,7 +14,7 @@ class CompletionJsonSerializer < BaseSerializer
   end
 end
 
-class CompletionHALSerializer < CompletionJsonSerializer
+class CompletionHALSerializer < BaseSerializer
   adapter Oat::Adapters::HAL
   schema do
     type "completion"
@@ -61,50 +61,3 @@ class CompletionHALSerializer < CompletionJsonSerializer
     end
   end
 end
-
-# class CompletionSerializer < BaseSerializer
-#
-#   schema do
-#     type "completion"
-#     link :self,        link_self(item[:id])
-#     link :describedby, link_described_by(item[:id])
-#     properties do |p|
-#       p.type      item[:type]
-#       p.label     item[:label]
-#       p.value     item[:value]
-#       p.highlight item[:highlight]
-#       p.actions   item[:actions]
-#     end
-#   end
-#
-#   private
-#
-#   def link_self(id)
-#     {
-#       :type => 'completion',
-#       :href => "#{base_url}/api/expressions/#{id}/completions"
-#     }
-#   end
-#
-#   def link_described_by(type, id)
-#     case type
-#     when :function
-#       {
-#         :type => 'function',
-#         :href => "#{base_url}/api/functions/#{id}"
-#       }
-#     when :namespace_prefix
-#       {
-#         :type => 'namespace_prefix',
-#         :href => "#{base_url}/api/namespaces/#{id}"
-#       }
-#     when :namespace_value
-#       {
-#         :type => 'namespace_value',
-#         :href => "#{base_url}/api/namespaces/hgnc/#{id}"
-#       }
-#     else
-#       raise NotImplementedError.new("Unexpected resource type, #{type}")
-#     end
-#   end
-# end
