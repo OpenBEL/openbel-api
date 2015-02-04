8c919e6456ab487277d884b1235e1fb19ee82a60
Anthony Bargnesi <abargnesi@gmail.com>
2015-02-04 13:48:23 -0500

    return 501 for syntax/semantic validations

--- app/routes/expressions.rb
+++ app/routes/expressions.rb
@@ -18,28 +18,13 @@ module OpenBEL
       # BEL Syntax Validation
       # TODO Move out to a separate route.
       get '/api/expressions/*/syntax-validations/?' do
-        bel = params[:splat].first
-        halt 400 unless bel
-
-        response.headers['Content-Type'] = 'application/json'
-
-        completions = BEL::Completion.complete(bel)
-        if completions.length == 1
-          is_terminal = completions.first.class == BEL::Completion::Terminal
-          MultiJson.dump [ valid: is_terminal ]
-        else
-          MultiJson.dump [ value: false ]
-        end
+        halt 501
       end
 
       # BEL Semantic Validations
       # TODO Move out to a separate route.
       get '/api/expressions/*/semantic-validations/?' do
-        bel = params[:splat].first
-        halt 400 unless bel
-
-        response.headers['Content-Type'] = 'application/json'
-        MultiJson.dump [ valid: "i'll allow it, but don't get punchy!" ]
+        halt 501
       end
 
     end
