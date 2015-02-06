92df21571b9acf04e2cac24d2ac5b8f3ddf3a67b
Anthony Bargnesi <abargnesi@gmail.com>
2015-02-05 21:46:21 -0500

    gosh; docs build and api test
    continuation of @nbargnesi gosh/node changes

--- tools/scripts/07-docs-build.sh
+++ tools/scripts/07-docs-build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # The next three lines are for the go shell.
-export SCRIPT_NAME="raml2html"
-export SCRIPT_HELP="Run raml2html over our RAML spec."
+export SCRIPT_NAME="build docs"
+export SCRIPT_HELP="Bulid documentation."
 [[ "$GOGO_GOSH_SOURCE" -eq 1 ]] && return 0
 
 # Normal script execution starts here.
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../../
 . "$DIR"/env.sh || exit 1
 
-"$SCRIPTS"/raml2html.sh
+"$SCRIPTS"/docs-build.sh
 
--- tools/scripts/api-test.sh
+++ tools/scripts/api-test.sh
@@ -3,8 +3,18 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../../
 cd "$DIR" || exit 1
 . "$DIR"/env.sh || exit 1
+use_gosh_contrib || exit 1
+assert_env TESTS || exit 1
 
-# requirements
+# Root of where node_modules will go
+export GOSH_CONTRIB_NODE_NPM_MODPATH="$TESTS"
+export GOSH_CONTRIB_NODE_NPM_PKGJSON="$TESTS"/package.json
+
+# Create the node environment if needed...
+create_node_env
+# ... and use it.
+export PATH="$GOSH_CONTRIB_NODE_NPM_MODPATH/node_modules/.bin":$PATH
+
 require_cmd curl
 
 SERVER_AS_DAEMON=1
@@ -19,6 +29,8 @@ while [ $EC -ne 0 ]; do
   curl -s "$TEST_API_ROOT_URL" 2>&1 > /dev/null
   EC=$?
 done
+
+echo "RUN TESTS"
 
 # run test against api specification
 "$TESTS_API_SPEC"/test.sh
