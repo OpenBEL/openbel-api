9ccfb515a21207934ab4ed4bb3f703c409f99424
Anthony Bargnesi <abargnesi@gmail.com>
2015-02-09 13:06:14 -0500

    parse object literals correctly; closes #22

--- lib/storage/triple_storage.rb
+++ lib/storage/triple_storage.rb
@@ -11,7 +11,7 @@ module OpenBEL
           [
             statement.subject.to_s[1..-2],
             statement.predicate.to_s[1..-2],
-            statement.object.to_s[1..-2]
+            object(statement)
           ]
         else
           statement
@@ -29,8 +29,12 @@ module OpenBEL
       end
 
       def object(statement)
-        return statement.object.value.to_s if statement.respond_to? :object
-        statement[2]
+        if statement.respond_to? :object
+          object = statement.object
+          (object.literal? && object.value.to_s) || object.to_s[1..-2]
+        else
+          statement[2]
+        end
       end
     end
   end
