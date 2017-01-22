fdac59fa7874f153e4e4fa0fdd2297850ad6e914
Anthony Bargnesi <abargnesi@gmail.com>
2017-01-22 16:34:31 -0500

    obtain identifier/pref_label if annotation found

--- app/openbel/api/resources/nanopub_transform.rb
+++ app/openbel/api/resources/nanopub_transform.rb
@@ -91,9 +91,9 @@ module OpenBEL
               }
             else
               annotation_value = annotation.find(value).first
-              identifier       = annotation_value.identifier.first.to_s
-              value_label      = annotation_value.pref_label.first.to_s
               if annotation_value
+                identifier  = annotation_value.identifier.first.to_s
+                value_label = annotation_value.pref_label.first.to_s
                 {
                   :name  => annotation_label,
                   :value => value_label,
